46079fdde2347078a3104c62b70dba4671262b95
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-26 20:04:11 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import teams from "./teams.json";
+import Dreamteam from "./component/Dreamteam"
+class App extends React.Component {
+  render() {
+    let teamed = teams.map(
+      team =>
+      (<Dreamteam shortName={team.shortName} picture={team.picture} stade={team.stade} address={team.address} email={team.email} phone={team.phone}
+        fundationDate={team.founded} />
+      )
+    )
+    return (
+      <div>
+        <ul>
+          <li>{teamed}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/component/Dreamteam.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Dreamteam extends React.Component {
+    render() {
+        return (
+            <>
+            
+            </>
+        )
+
+    }
+}
+
+export default Dreamteam
