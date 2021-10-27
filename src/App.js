c4ba9f0cc4cd72b9b818a4a24ac779a67e2c42cb
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-27 09:19:08 +0200

    Affichage des differentes equipes de Football

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import teams from "./teams.json";
 import Dreamteam from "./component/Dreamteam"
+
 class App extends React.Component {
   render() {
-    let teamed = teams.map(
+    let mappedTeam = teams.map(
       team =>
-      (<Dreamteam shortName={team.shortName} picture={team.picture} stade={team.stade} address={team.address} email={team.email} phone={team.phone}
-        fundationDate={team.founded} />
+      (<Dreamteam shortName={team.shortName} picture={team.crestUrl} stade={team.venue} address={team.address} email={team.email} phone={team.phone}
+        foundationDate={team.founded} />
       )
     )
+
     return (
-      <div>
-        <ul>
-          <li>{teamed}</li>
-        </ul>
+      <div> 
+        {mappedTeam}  
       </div>
+      
+
     )
   }
 }
--- src/component/Dreamteam.js
+++ src/component/Dreamteam.js
@@ -3,9 +3,15 @@ import React from "react"
 class Dreamteam extends React.Component {
     render() {
         return (
-            <>
-            
-            </>
+            <div>
+                <img src={this.props.picture} />
+                <h2>{this.props.shortName}</h2>
+                <p>{this.props.stade}</p>
+                <p>{this.props.foundationDate}</p>
+                <p>{this.props.address}</p>
+                <p>{this.props.email}</p>
+                <p>{this.props.phone}</p>
+            </div>
         )
 
     }
